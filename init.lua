ccc844439e40cc4bfece293e0ad5dc3d1c0af00c
mhelgestad <maxhelgestad6@outlook.com>
2025-08-19 07:57:55 -0500

    color options

--- init.lua
+++ init.lua
@@ -1,5 +1,14 @@
 -- theme & transparency
 vim.cmd.colorscheme("unokai")
+-- OTHER OPTIONS
+-- slate
+-- habamax
+-- retrobox
+-- sorbet
+-- wildcharm
+-- zaibatsu
+-- 
+--
 -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 -- vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
